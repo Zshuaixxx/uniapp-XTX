777c9eb3fed7d7a1c3e2a27e9d82a3efb908ef5a
Zshuaixxx <z_20058@qq.com>
2024-07-19 14:16:28 +0800

    分类页基本结构和swiper组件在页面的使用

--- uniappXTX/unpackage/dist/dev/mp-weixin/pages/classify/classify.js
+++ uniappXTX/unpackage/dist/dev/mp-weixin/pages/classify/classify.js
@@ -1,8 +1,45 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
+const api_home = require("../../api/home.js");
+if (!Math) {
+  XtxSwiper();
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
-wx.createPage(MiniProgramPage);
+const XtxSwiper = () => "../../components/XtxSwiper.js";
+const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
+  __name: "classify",
+  setup(__props) {
+    common_vendor.onLoad(() => {
+      getSwiperData();
+    });
+    const swiperData = common_vendor.ref();
+    const getSwiperData = async () => {
+      const res = await api_home.getHomeBannerService(2);
+      swiperData.value = res.result;
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.f(10, (item, index, i0) => {
+          return {
+            a: item,
+            b: index === 0 ? 1 : ""
+          };
+        }),
+        b: common_vendor.p({
+          list: swiperData.value
+        }),
+        c: common_vendor.f(3, (item, k0, i0) => {
+          return {
+            a: common_vendor.f(4, (goods, k1, i1) => {
+              return {
+                a: goods
+              };
+            }),
+            b: item
+          };
+        }),
+        d: `/pages/goods/goods?id=`
+      };
+    };
+  }
+});
+wx.createPage(_sfc_main);
